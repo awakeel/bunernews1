8beaf3f05fdf1d408abdb7a77f12fc60c1068339
Abdul wakeel <itsprogrammer@gmail.com>
2015-03-02 00:29:50 +0500

    c
    c

---
+++ www/js/employeelist.js
@@ -0,0 +1,54 @@
+var serviceURL = "http://localhost/emp/services/";
+
+var employees;
+
+$('#employeeListPage').bind('pageinit', function(event) {
+	getEmployeeList();
+});
+function getFacebook(){
+ $('#employeeList li').remove();
+		  $.ajaxSetup({ cache: true }); 
+		    FB.init({
+		      appId: '772672609440031',
+		    });     
+		    $('#loginbutton,#feedbutton').removeAttr('disabled');
+		   /// FB.getLoginStatus(updateStatusCallback);	
+		  
+	   FB.api(
+    	"/454855137937533/feed",
+  	 	 	function (responses) {
+    	  if (responses && !responses.error) {
+       		 $.each(responses, function(index, response) {
+       		 	var response = response[0];
+       		 	var image = "";
+       		 	var id = 11;
+       		 	 console.log(response.id);
+       		 	$('#employeeList').append('<li><a href="employeedetails.html?id=' +  id + '">' +
+					image+
+					'<h4>' + response.message + '</h4>' +
+					'<p>' + response.created_time + '</p>' +
+					'<span class="ui-li-count">' + response.from.name + '</span></a></li>');
+       		 	 $('#employeeList').listview('refresh');
+       		 });
+       		
+    	      }
+   			}
+		);	   
+    }
+function getEmployeeList() {
+
+	 getFacebook();
+	 return;
+	$.getJSON(serviceURL + 'getemployees.php', function(data) {
+		$('#employeeList li').remove();
+		employees = data.items;
+		$.each(employees, function(index, employee) {
+			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+					'<img src="pics/' + employee.picture + '"/>' +
+					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+					'<p>' + employee.title + '</p>' +
+					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+		});
+		$('#employeeList').listview('refresh');
+	});
+}
